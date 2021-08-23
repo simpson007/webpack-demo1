37a7c7ab9360e0138f1d5e7d1325fce542b6c2b8
simpson <13951719485@163.com>
2021-08-23 21:31:18 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode:"production",
     entry: './src/index.js',
     output: {
-        filename: 'index.js'
-    }
+        filename: 'index.[contenthash].js'
+    },plugins: [new HtmlWebpackPlugin(
+        {
+            title: "webpack-demo1",
+            template: "src/assets/index.html"
+        }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 }
