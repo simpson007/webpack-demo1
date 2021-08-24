f6f2276af9a10b0b61ca71262a218cfb6d026701
simpson <13951719485@163.com>
2021-08-24 21:45:18 +0800

    Ready to upload

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -5,10 +5,39 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'index.[contenthash].js'
-    },plugins: [new HtmlWebpackPlugin(
+    }, plugins: [new HtmlWebpackPlugin(
         {
             title: "webpack-demo1",
             template: "src/assets/index.html"
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.styl$/,
+                loader: ['style-loader','css-loader','stylus-loader'],
+            },
+            {
+                test: /\.less$/,
+                loader: ['style-loader','css-loader','less-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('dart-sass')
+                        }
+                    },
+                ],
+            },
+        ],
+    },
 }
